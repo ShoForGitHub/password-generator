0153222b71807c4159269888aec214366bc5911c
Sho <shohihi@hotmail.com>
2024-03-16 15:12:13 +0900

    Refactor

--- index.js
+++ index.js
@@ -3,32 +3,50 @@ let option = {
   numbers : ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
   symbols : ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"],
 }
-let characters = [];
-characters = characters.concat(option.alphabets);
-
+let characters;
 let generateBtn = document.querySelector("#generate-btn");
-let Passwords = document.querySelectorAll(".password-text");
+let passwords = document.querySelectorAll(".password-text");
 let numbersSwitch = document.querySelector("#numbers-switch");
 let symbolsSwitch = document.querySelector("#symbols-switch");
 
+initialPassword();
 toggleJudge(numbersSwitch);
 toggleJudge(symbolsSwitch);
 
 generateBtn.addEventListener("click", function() {
-  Passwords.forEach((item) => item.textContent = generatePassword());
+  passwords.forEach((item) => item.textContent = generatePassword());
 })
 
-function toggleJudge(switchToggle) {
-  switchToggle.addEventListener("change", function() {
+// click to copy
+passwords.forEach(function(password) {
+  password.addEventListener("click", function() {
+    const textToCopy = password.innerText;
+    navigator.clipboard.writeText(textToCopy)
+      .then(function() {
+        alert("Copied: " + textToCopy);
+      }).catch(function(error) {
+        alert("Sorry, failed to copy.");
+        console.error("Failed to copy:", error);
+      });
+  });
+});
+
+function initialPassword() {
   characters = [];
   characters = characters.concat(option.alphabets);
+}
+
+//whether to include numbers and symbols
+function toggleJudge(switchToggle) {
+  switchToggle.addEventListener("change", function() {
+  initialPassword();
   if (numbersSwitch.checked === true) {
     characters = characters.concat(option[numbersSwitch.name]);
   }
   if (symbolsSwitch.checked === true) {
     characters = characters.concat(option[symbolsSwitch.name]);
   }
-})
+  })
 }
 
 function generatePassword() {
@@ -44,19 +62,3 @@ function getPasswordLength() {
   let passwordLength = document.getElementById("password-length").value;
   return passwordLength;
 }
-
-// click to copy
-let passwordTexts = document.querySelectorAll(".password-text");
-
-passwordTexts.forEach(function(passwordText) {
-  passwordText.addEventListener("click", function() {
-    const textToCopy = passwordText.innerText;
-    navigator.clipboard.writeText(textToCopy)
-      .then(function() {
-        alert("Copied: " + textToCopy);
-      }).catch(function(error) {
-        alert("Sorry, failed to copy.")
-        console.error("Failed to copy:", error);
-      });
-  });
-});
